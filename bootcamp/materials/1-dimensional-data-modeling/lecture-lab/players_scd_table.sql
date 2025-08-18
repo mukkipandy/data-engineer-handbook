fe63c8251e8fb2b71040bc853b61c7fb52af1ccd
Mukesh Pandey <mukesh.mtr@gmail.com>
2025-08-18 12:34:40 +0530

    sql corected

--- bootcamp/materials/1-dimensional-data-modeling/lecture-lab/players_scd_table.sql
+++ bootcamp/materials/1-dimensional-data-modeling/lecture-lab/players_scd_table.sql
@@ -1,10 +1,11 @@
-create table players_scd_table
+create table players_scd
 (
 	player_name text,
 	scoring_class scoring_class,
 	is_active boolean,
 	start_season integer,
-	end_date integer,
-	current_season INTEGER
+	end_season integer,
+	current_season INTEGER,
+	PRIMARY KEY (player_name, start_season)
 );
 
